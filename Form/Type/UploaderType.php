67bec29f92eb906811c9a6a65e5fae8275c45cae
excelwebzone <excelwebzone@gmail.com>
2012-07-23 06:14:36 -0700

    Fixes typo - wrong class

--- Form/Type/UploaderType.php
+++ Form/Type/UploaderType.php
@@ -5,7 +5,7 @@ namespace EWZ\Bundle\UploaderBundle\Form\Type;
 use Symfony\Component\Routing\Router;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormViewInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Form\Options;
 
@@ -52,7 +52,7 @@ class UploaderType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function buildView(FormViewInterface $view, FormInterface $form, array $options)
+    public function buildView(FormView $view, FormInterface $form, array $options)
     {
         $view->vars = array_replace($view->vars, array(
             'max_size'     => $options['max_size'],
